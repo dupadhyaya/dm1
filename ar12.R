55323981438dd3a122606a551b6afc01174cbec2
dupadhyaya <dup1966@gmail.com>
2016-12-01 08:29:23 +0530

    d

--- ar12.R
+++ ar12.R
@@ -8,34 +8,36 @@ dim(df_groceries)
 #read transactions
 
 df_sorted <- df_groceries[order(df_groceries$Member_number),]
-
+df_sorted
 
 #convert member number to numeric
 df_sorted$Member_number <- as.numeric(df_sorted$Member_number)
-
-
-#convert item description to categorical format
-
-df_sorted$itemDescription <- as.factor(df_sorted$itemDescription)
-
 str(df_sorted)
-
+head(df_sorted)
+#convert item description to categorical format
+df_sorted$itemDescription <- as.factor(df_sorted$itemDescription)
+str(df_sorted)
 #convert dataframe to transaction format using ddply; 
 
-if(sessionInfo()['basePkgs']=="dplyr" | sessionInfo()['otherPkgs']=="dplyr"){
-  detach(package:dplyr, unload=TRUE)
-}
+#if(sessionInfo()['basePkgs']=="dplyr" | sessionInfo()['otherPkgs']=="dplyr"){
+#  detach(package:dplyr, unload=TRUE)
+#}
+library(dplyr)
 
 #group all the items that were bought together; by the same customer on the same date
 library(plyr)
+?ddply
+
 df_itemList <- ddply(df_groceries, c("Member_number","Date"), function(df1)paste(df1$itemDescription,collapse = ","))
 
 #remove member number and date
 df_itemList$Member_number <- NULL
 df_itemList$Date <- NULL
-
+str(df_itemList)
 colnames(df_itemList) <- c("itemList")
-
+str(df_itemList)
+head(df_itemList,n=20)
+df_itemList
 #write to csv format
 write.csv(df_itemList,"ItemList.csv", row.names = TRUE)
 
@@ -44,16 +46,19 @@ write.csv(df_itemList,"ItemList.csv", row.names = TRUE)
 #load package required
 library(arules)
 
-#convert csv file to basket format
+#convert csv file to basket format - transactions
 txn = read.transactions(file="ItemList.csv", rm.duplicates= TRUE, format="basket",sep=",",cols=1);
-
+txn = read.transactions(file.choose(), rm.duplicates= TRUE, format="basket",sep=",",cols=1);
+txn
+inspect(txn)
 #remove quotes from transactions
 txn@itemInfo$labels <- gsub("\"","",txn@itemInfo$labels)
 
 
 #run apriori algorithm
-basket_rules <- apriori(txn,parameter = list(minlen=2,sup = 0.01, conf = 0.1, target="rules"))
+basket_rules <- apriori(txn,parameter = list(minlen=2,sup = 0.01, conf = 0.7, target="rules"))
 #basket_rules <- apriori(txn,parameter = list(minlen=2,sup = 0.00001, conf = 0.01, target="rules"),appearance = list(lhs = "CLEMENTINES")))
+inspect(basket_rules)  # no rule
 
 #check if tm is attched; if yes then detach
 if(sessionInfo()['basePkgs']=="tm" | sessionInfo()['otherPkgs']=="tm"){
--- roughAR.R
+++ roughAR.R
@@ -14,3 +14,9 @@ as(lst1, "transactions")
 #transactions in sparse format with 
 # 7 transactions (rows) and 
 # 48842 items (columns) 
+
+library(package = "arules")
+v = read.csv(file = file('stdin'))
+transactions = read.transactions(file = file("stdin"), format = "basket", sep = ",")
+#a,b
+#a,b,c
